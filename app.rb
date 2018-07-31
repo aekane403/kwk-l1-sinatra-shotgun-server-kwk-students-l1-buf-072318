0d376d24cd9f182a667b304bca6e05538d222ab7
Abigail Kane <aekane403@gmail.com>
2018-07-31 18:41:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I'm trying to make a change"
+    "Welcome to your app!!!! I'm trying to make a change."
   end
 
 end
